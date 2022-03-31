987d9070139ea528a706107cb01376b4237e6fca
Mariusz Kryński <mrk@sed.pl>
2022-03-31 23:41:52 +0200

    update deploy_test.sh

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,18 +1,18 @@
 set -e
 set -x
-if [[ -n $(git status -s | grep -v '??' | grep -v bevy-atari-antic) ]]; then
+if [[ -n $(git status -s | grep -v '??' | grep -v deploy_test.sh | grep -v bevy-atari-antic) ]]; then
   echo git not clean, qutting
   exit 1
 fi
 
 DEST=test.tmp
-cargo make build-web -p release
+cargo make build-webgl -p release
 mkdir -p $DEST/target
 mkdir -p $DEST/pokey
 
-cp -v index.html $DEST
-mv -v target/wasm_bg.wasm target/wasm.js $DEST/target
-cp -v pokey/pokey.js $DEST/pokey
-cp -v -a js $DEST
+cp -v web/index.html $DEST
+cp -v -a web/wasm $DEST/wasm
+cp -v web/pokey/pokey.js $DEST/pokey
+cp -v -a web/js $DEST
 
 #git checkout web && cp -av test.tmp/* test/
